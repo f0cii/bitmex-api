b6f95147a41b0de61b3f1d78fa2ef68fbe88005f
sumorf <529808348@qq.com>
2019-04-12 13:45:08 +0800

    Add test config

--- api_test.go
+++ api_test.go
@@ -1,9 +1,21 @@
 package bitmex
 
-import "os"
+import (
+	"github.com/micro/go-config"
+	"github.com/micro/go-config/source/file"
+	"os"
+	"testing"
+)
 
 const (
 	proxyURL = "http://127.0.0.1:1080"
+)
+
+var (
+	_proxyURL string
+	_testnet  bool
+	_key      string
+	_secret   string
 )
 
 func useProxy() {
@@ -11,11 +23,50 @@ func useProxy() {
 	os.Setenv("HTTPS_PROXY", proxyURL)
 }
 
-func newBitmexForTest() *BitMEX {
-	useProxy()
+func loadConfig() {
+	err := config.Load(file.NewSource(file.WithPath("./testdata/config.yaml")))
+	if err != nil {
+		return
+	}
+	cfg := config.Map()
 
-	key := `8K2Oi0bnRRZ7GK4UJnY-38oj`
-	secret := `9EmGvk8mKX5nWa11y1KyPPGn78Lv2ZEiLx3TH0YasE_oE06y`
-	bitmex := New(BitmexTestnetHost, key, secret, "XBTUSD")
+	_proxyURL = cfg["proxy_url"].(string)
+	_testnet = cfg["testnet"].(bool)
+	_key = cfg["key"].(string)
+	_secret = cfg["secret"].(string)
+}
+
+func TestConfig(t *testing.T) {
+	err := config.Load(file.NewSource(file.WithPath("./testdata/config.yaml")))
+	if err != nil {
+		t.Error(err)
+	}
+	cfg := config.Map()
+
+	proxy := cfg["proxy"].(string)
+	testnet := cfg["testnet"].(bool)
+	key := cfg["key"].(string)
+	secret := cfg["secret"].(string)
+
+	t.Log(proxy)
+	t.Log(testnet)
+	t.Log(key)
+	t.Log(secret)
+}
+
+func newBitmexForTest() *BitMEX {
+	loadConfig()
+	if _proxyURL != "" {
+		os.Setenv("HTTP_PROXY", _proxyURL)
+		os.Setenv("HTTPS_PROXY", _proxyURL)
+	}
+
+	var host string
+	if _testnet {
+		host = BitmexTestnetHost
+	} else {
+		host = BitmexHost
+	}
+	bitmex := New(host, _key, _secret, "XBTUSD")
 	return bitmex
 }
